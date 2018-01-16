988f31258d6dd13e78e87714df50ee3b713e799e
Surender Singh <surender.s@flipkart.com>
2018-01-16 21:43:14 +0530

    JIRA: EKLTRANS-1224: Service Up

---
+++ src/main/java/com/sample/school/management/SchoolManagementApplication.java
@@ -0,0 +1,14 @@
+package com.sample.school.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by surender.s on 16/01/18.
+ */
+@SpringBootApplication
+public class SchoolManagementApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(SchoolManagementApplication.class, args);
+    }
+}
